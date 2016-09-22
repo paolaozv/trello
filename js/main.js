586f06069e00bd364a612d4c1eef508342ce81d1
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-09-22 11:32:53 -0500

    muestra en html texto añadido

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function() {
 
     var contenedor = document.getElementById("contenedor");
     var box = document.getElementById("box");
+    var formulario = document.getElementById("formulario");
     
     box.addEventListener("click", function() {
         addTag();
@@ -10,22 +11,44 @@ window.addEventListener("load", function() {
     function addTag() {
         box.style.display = "none";
 
-        var formulario = document.createElement("form");
-        contenedor.insertBefore(formulario, contenedor.childNodes[0]);
-
-        var publicacion = document.createElement("input");
-        formulario.insertBefore(publicacion, formulario.childNodes[0]).classList.add("write");
+        var entrada = document.createElement("input");
+        formulario.insertBefore(entrada, formulario.childNodes[0]);
+        entrada.classList.add("write");
+        entrada.setAttribute("type", "text");
+        entrada.setAttribute("placeholder", "Añadir una lista...")
 
         var boton = document.createElement("button");
-        var texto = document.createTextNode("Guardar");
-        boton.appendChild(texto);
-        formulario.insertBefore(boton, formulario.childNodes[1]).classList.add("boton");
+        var textoBoton = document.createTextNode("Guardar");
+        boton.appendChild(textoBoton);
+        formulario.insertBefore(boton, formulario.childNodes[1]);
+        boton.classList.add("boton");
+        boton.setAttribute("type", "submit");
 
         var icon = document.createElement("icon");
-        var style = formulario.insertBefore(icon, formulario.childNodes[2])
-        style.classList.add("icon-cross");
-        style.classList.add("iconPosition");
+        formulario.insertBefore(icon, formulario.childNodes[2]);
+        icon.classList.add("icon-cross");
+        icon.classList.add("iconPosition");
 
         contenedor.classList.add("caja");
+
+        boton.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            entrada.style.display = "none";
+            boton.style.display = "none";
+            icon.style.display = "none";
+
+            var texto = entrada.value;
+            var titulo = document.createElement("div");
+            titulo.innerHTML = texto;
+            formulario.insertBefore(titulo, formulario.childNodes[0]);
+            titulo.classList.add("titulo");
+
+            var enlace = document.createElement("a");
+            var textoEnlace = document.createTextNode("Añadir una tarjeta...");
+            enlace.appendChild(textoEnlace);
+            formulario.insertBefore(enlace, formulario.childNodes[1]);
+            enlace.classList.add("enlace");
+        });
     }
 });                     
